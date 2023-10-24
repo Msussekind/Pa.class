86e9dabf993c98a3788678c85260cc5fb16cdf19
Msussekind <141173177+Msussekind@users.noreply.github.com>
2023-10-24 14:38:03 -0300

    Add files via upload

---
+++ trab.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def simples (cor):
+    if cor == 'azul':
+        return 'escolheu certo'
+    
+def medio (cor):
+    if cor == 'branco':
+        return 'escolheu certo'
+    else: 
+        return 'tente outra cor'
+    
+def completo (cor):
+    if cor == 'preto': 
+        return 'escolheu certo'
+    elif cor == 'amarelo': 
+        return 'não tem salvação'
+    else: 
+        return 'tente outra cor'
+    
